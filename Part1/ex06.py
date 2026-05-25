ac1382ec58e9848c69fa3c3df5918d0e2305f7b6
Lawrencelow088 <m-9444902@moe-dl.edu.my>
2026-05-25 05:25:14 +0000

    “done”

---
+++ Part1/ex06.py
@@ -0,0 +1,16 @@
+# List of names
+names = ['Tom', 'Jerry', 'Butch', 'Tuffy', 'Pecos']
+
+# TODO: Try the following:
+# Print the whole list
+# Print the first name
+# Print the last name
+# Print the middle two names using list slicing
+thislist = ('Tom', 'Jerry', 'Butch', 'Tuffy', 'Pecos')
+print(thislist)
+# Access the first item
+print(names[0])  # Output: Tom
+# Access the last item
+print(names[1])  # Output: Pecos
+# Access the middle two names
+print(names[2:4]) # Output: Butch,Tuffy 
