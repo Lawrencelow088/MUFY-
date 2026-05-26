6e30017e71c50316fcace9423faf8cd13416ca1f
Lawrencelow088 <m-9444902@moe-dl.edu.my>
2026-05-26 04:45:37 +0000

    “done”

---
+++ Part2/ex14.py
@@ -0,0 +1,22 @@
+# List of countries
+countries = ['Malaysia', 'Japan', 'Armenia', 'Brazil', 'Australia']
+
+# TODO:
+# Create a for loop that goes through the list and prints each country
+
+
+# Insert your code here
+for x in countries:
+    if x == "Malaysia":
+      print("Come and visit Malaysia")
+    if x == "Japan":
+      print("Come and visit Japan")
+    if x == "Armenia":
+      print("Come and visit Armenia")
+    if x == "Brazi;":
+      print("Come and visit Brazil")
+    if x == "Australia":
+      print("Come and visit Australia")
+
+
+
---
+++ Part2/ex15.py
@@ -0,0 +1,15 @@
+import random
+
+# List of flowers
+flowers = ['rose', 'tulip', 'lily']
+num =[1,101]
+
+# 🧩 TODO:
+# 1. Print a random flower from the list
+# 2. Print a random number from 0 to 100
+
+# Insert your code here
+selected = random.choice (flowers)
+print(selected)# could be any three
+num = random.randrange (1,100)
+print(num)# could be any num
